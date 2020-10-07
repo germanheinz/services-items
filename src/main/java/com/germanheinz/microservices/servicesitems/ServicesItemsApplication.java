a4dd9bb73b3870ae5e074c0a56610a0633156978
germanheinz <heinz.german@gmail.com>
2020-10-07 18:08:59 +0200

    Eureka Server - Config

--- src/main/java/com/germanheinz/microservices/servicesitems/ServicesItemsApplication.java
+++ src/main/java/com/germanheinz/microservices/servicesitems/ServicesItemsApplication.java
@@ -2,11 +2,11 @@ package com.germanheinz.microservices.servicesitems;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
-@RibbonClient(name = "services-products")
+@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
 public class ServicesItemsApplication {
