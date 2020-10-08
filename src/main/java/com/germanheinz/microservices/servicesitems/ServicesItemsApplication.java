aefca178477a1982404487ad8a0eea8d507bfe95
germanheinz <heinz.german@gmail.com>
2020-10-08 12:21:18 +0200

    Hystrix - Deal with problem, with circuit breaker - alternative method

--- src/main/java/com/germanheinz/microservices/servicesitems/ServicesItemsApplication.java
+++ src/main/java/com/germanheinz/microservices/servicesitems/ServicesItemsApplication.java
@@ -2,10 +2,13 @@ package com.germanheinz.microservices.servicesitems;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
+@EnableCircuitBreaker
 @EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
--- src/main/java/com/germanheinz/microservices/servicesitems/controller/ItemController.java
+++ src/main/java/com/germanheinz/microservices/servicesitems/controller/ItemController.java
@@ -1,13 +1,16 @@
 package com.germanheinz.microservices.servicesitems.controller;
 
 import com.germanheinz.microservices.servicesitems.models.Item;
+import com.germanheinz.microservices.servicesitems.models.Product;
 import com.germanheinz.microservices.servicesitems.services.ItemService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -22,9 +25,21 @@ public class ItemController {
         return itemService.findAll();
     }
 
+    @HystrixCommand(fallbackMethod = "alternativeMethod")
     @GetMapping("/detail/{id}/quantity/{quantity}")
     public Item getDetail(@PathVariable Long id, @PathVariable Integer quantity){
         return  itemService.findById(id, quantity);
+    }
+
+
+    public Item alternativeMethod(Long id, Integer quantity, Throwable hystrixCommand){
+        Product pro = new Product();
+        pro.setName("test");
+        pro.setCreatedDate(new Date());
+        pro.setId(id);
+        pro.setPrice(1.0);
+
+        return new Item(pro, quantity);
     }
 
 }
